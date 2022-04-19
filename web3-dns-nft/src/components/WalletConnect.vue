7b42ed71a41d81f1e00947a58f1e110f8b1bef7d
mrth2 <mr.th2.90@gmail.com>
2022-04-19 23:57:43 +0700

    add component to connect meta mask walllet

---
+++ web3-dns-nft/src/components/WalletConnect.vue
@@ -0,0 +1,49 @@
+<script setup>
+defineProps({
+  currentAccount: {
+    type: String,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["update:account"]);
+
+async function connectWallet() {
+  const { ethereum } = window;
+  try {
+    if (!ethereum) {
+      alert("Please install MetaMask");
+      return;
+    }
+    const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+    emit("update:account", accounts[0]);
+    console.log("account: ", accounts[0]);
+  } catch (err) {
+    console.log("error", err);
+    alert(err.message);
+  }
+}
+</script>
+
+<template>
+  <div class="cw-container">
+    <iframe
+      src="https://giphy.com/embed/rPbVaGPnheoQ2dFkND"
+      width="200"
+      height="200"
+      frameBorder="0"
+      class="giphy-embed"
+      allowFullScreen
+    />
+    <button @click="connectWallet">Connect Wallet</button>
+  </div>
+</template>
+
+<style scoped lang="postcss">
+.cw-container {
+  @apply flex flex-col justify-center items-center;
+}
+button {
+  @apply bg-green-600 px-4 py-2 rounded mt-5 text-white font-bold;
+}
+</style>
